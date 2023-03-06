3575c4f0d6abba0bd736039736c1665789d717fa
Arun Anand <arunanand.tech@gmail.com>
2023-03-06 08:35:37 +0300

    Restaurant card updated

---
+++ src/Componets/Restop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Restop(props) {
+console.log(props.operate);
+  return (
+    <div>
+        Operating Hours
+    </div>
+  )
+}
+
+export default Restop
--- src/Componets/Viewrest.js
+++ src/Componets/Viewrest.js
@@ -1,10 +1,63 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useEffect,useState } from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Image } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Restop from './Restop';
 
 function Viewrest() {
+
+  const urlParams = useParams();
+  console.log(urlParams.id);
+
+  //Create a state
+  const [AllRestaurants,setAllRestaurants] = useState([])
+
+  //To fetch data
+  const getRestaurants = async()=>{
+      await fetch('/restaurants.json').then((data)=>{
+          data.json().then(
+              (result)=>{
+                  setAllRestaurants(result.restaurants)
+              }
+          )
+      })
+  }
+  // console.log(AllRestaurants);
+
+  useEffect(()=>{
+      getRestaurants()
+  },[])
+
+  const viewrest = AllRestaurants.find(item=>item.id=urlParams.id)
+  console.log(viewrest);
+
   return (
-    <div>
-        Hello Guys
-    </div>
+    <>
+      {
+        viewrest?(
+          <Row className='m-3'>
+            <Col md={4}>
+            <Image src={viewrest.photograph} fluid/>
+            </Col>
+
+            <Col md={7}>
+            <ListGroup>
+              <ListGroup.Item><h1>{viewrest.name}</h1></ListGroup.Item>
+              <ListGroup.Item>{viewrest.cuisine_type}</ListGroup.Item>
+              <ListGroup.Item><h3>{viewrest.neighborhood}</h3></ListGroup.Item>
+              <ListGroup.Item><h3>{viewrest.address}</h3></ListGroup.Item>
+
+              <ListGroup.Item><Restop operate={viewrest.operating_hours}/></ListGroup.Item>
+              </ListGroup>
+            </Col>
+
+          </Row>
+        ):'null'
+      }  
+    </>
   )
 }
 
